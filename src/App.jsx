0824414a78f3ec8d413f3cfb4229a50c97d87d7c
broknBRAIN <priyanshyadav99399@gmail.com>
2025-07-07 23:47:20 +0530

    event listener button

--- src/App.jsx
+++ src/App.jsx
@@ -4,51 +4,30 @@ import InputCard from "./component/InputCard"
 import UserCard from "./component/UserCard"
 
 
-//conditional rendering
-function App() {
-  const [show,setShow] = useState(false);
-
-  if(show)
-  {
-    const [name,setName] = useState('');
-    return (
-      <div>
-        <InputCard name={name} setName ={setName} />
-        <p>value of name in parent is {name}</p>
-      </div>
-    )
-  }
-  else{
-      return (
-        <div>
-          <UserCard name="Priyansh" desc="working"/>
-        </div>
-      )
-  }
+const handleClick = () => {
+  alert("i am clicked");
 }
-
-//depending upon the value of the show
-//we render what we want to show
-//ternary operator can also be used
+//event handling
+function App() {
+  
+  return (
+    <div>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+    //its so easy to perform operation on click
+  )
+}
 
 export default App
 
 
-//learning props.children
+//suppose we did event handling by java script
+//we need to fetch that object by class or id
+//then use event listener, then what type of event
+//lot of work
 
-//we have counter component
-
- /* <Counter props="anything">
-      anything over here is the props.children part
-    <Counter/> */
-
-//you can even pass functions in props
-//you can pass functions as children as well
-//see how the counter is implemented now
-
-//where do we use it
-//when we don't know how may parameters are there
-
-//we just call children in such a case, when we make a wrapper
-//wrapper means anything is given, and you just wrap it,
-//in wrapper we don't know how much is gonna come, so we use props.children
+//but in react
+//once you type on...in props area you get all the events that could be done
+//like mousehouwer click etc
+//its easy and intutive
+//inside the onevent(function to perform you pass)
